b1a425c90c2536a047068f1dc3544a986dc64176
pkyujin <luxoris02@sookmyung.ac.kr>
2026-03-24 23:11:44 +0900

    [Feat(제이/박유진)] 1주차 단일 화면 구성

--- app/src/main/java/com/example/umc_10th/MainActivity.kt
+++ app/src/main/java/com/example/umc_10th/MainActivity.kt
@@ -14,6 +14,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvMain.text = "Welcome UMC!"
+        binding.tvQuestion.text = "오늘 하루는 어땠나요?"
+        binding.tvSubtitle.text = "감정우표를 선택해주세요"
+        binding.tvDescription.text = "선택한 감정우표를 기반으로 맞춤형 질문이 배달됩니다"
+
+        binding.imgHappy.setImageResource(R.drawable.ic_happy)
+        binding.tvHappy.text = "더없이 행복한 하루였어요"
+        binding.imgExcited.setImageResource(R.drawable.ic_excited)
+        binding.tvExcited.text = "들뜨고 흥분돼요"
+        binding.imgNormal.setImageResource(R.drawable.ic_normal)
+        binding.tvNormal.text = "평범한 하루였어요"
+        binding.imgNervous.setImageResource(R.drawable.ic_nervous)
+        binding.tvNervous.text = "생각이 많아지고 불안해요"
+        binding.imgAngry.setImageResource(R.drawable.ic_angry)
+        binding.tvAngry.text = "부글부글 화가 나요"
+
     }
 }
