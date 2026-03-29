e7849d55b073a3727061e2d2a1dfa051f740de0a
pkyujin <luxoris02@sookmyung.ac.kr>
2026-03-29 15:41:30 +0900

    [Feat(제이/박유진)] 2주차 Activity 생명주기 점검 완료

--- week2/app/src/main/java/com/example/androidapp/MainActivity.kt
+++ week2/app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.androidapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val TAG = "LIFE_QUIZ"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Log.d(TAG, "onCreate")
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_fragmentContainer, HomeFragment())
@@ -58,5 +63,34 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
     }
 }
