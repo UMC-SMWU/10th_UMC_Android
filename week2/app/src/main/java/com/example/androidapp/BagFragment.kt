da354f7dbef96001cad30e62d22d77378e412d48
pkyujin <luxoris02@sookmyung.ac.kr>
2026-03-29 15:13:03 +0900

    [Feat(제이/박유진)] 2주차 Fragment Navigation 구현

--- week2/app/src/main/java/com/example/androidapp/BagFragment.kt
+++ week2/app/src/main/java/com/example/androidapp/BagFragment.kt
@@ -5,55 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.androidapp.databinding.FragmentBagBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BagFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BagFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentBagBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bag, container, false)
+    ): View {
+
+        _binding = FragmentBagBinding.inflate(inflater, container, false)
+
+
+        binding.myImage.setImageResource(R.drawable.ic_bagcircle)
+        binding.tvEmptyBag.text = "장바구니가 비어 있습니다.\n제품을 추가하면 여기에 표시됩니다."
+        binding.btnAddItem.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.main_fragmentContainer, ShoppingFragment())
+                .commit()
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BagFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BagFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
