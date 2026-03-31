338b2175b7cbb6935bdfab26c26d399d8913d63d
pkyujin <luxoris02@sookmyung.ac.kr>
2026-04-01 02:05:30 +0900

    [Feat(제이/박유진)] 3주차 Home RecyclerView 구현

--- week2/app/src/main/java/com/example/androidapp/HomeFragment.kt
+++ week2/app/src/main/java/com/example/androidapp/HomeFragment.kt
@@ -5,12 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val itemList = listOf(
+            ItemData(R.drawable.home_item1,"Air Jordan XXXVI", "US\$185"),
+            ItemData(R.drawable.home_item2,"Nike Air Force 1 '07", "US\$115")
+        )
+
+        val adapter = ItemAdapter(itemList)
+
+        binding.homeRecyclerView.adapter = adapter
+        binding.homeRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,7 +37,9 @@ class HomeFragment : Fragment() {
 
         binding.tvTitle.text = "Discover"
         binding.tvSubtitle.text = "3월 27일 금요일"
-        binding.myImage.setImageResource(R.drawable.home_logo)
+        // binding.myImage.setImageResource(R.drawable.home_logo)
+        binding.tvNewTitle.text = "What's new"
+        binding.tvNewSubtitle.text = "나이키 최신 상품"
 
         return binding.root
     }
---
+++ week2/app/src/main/java/com/example/androidapp/ItemData.kt
@@ -0,0 +1,7 @@
+package com.example.androidapp
+
+data class ItemData(
+    val icon: Int,
+    val name: String = "",
+    val price: String? = ""
+)
