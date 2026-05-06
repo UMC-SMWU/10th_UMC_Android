7055b813d7ef237302b37413814a6ca165a4ecb1
pkyujin <luxoris02@sookmyung.ac.kr>
2026-05-06 21:18:37 +0900

    [Refactor(제이/박유진)] 6주차 ViewModel 사용

--- week2/app/src/main/java/com/example/androidapp/UserFragment.kt
+++ week2/app/src/main/java/com/example/androidapp/UserFragment.kt
@@ -1,24 +1,34 @@
 package com.example.androidapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import coil.load
 import coil.transform.CircleCropTransformation
 import com.example.androidapp.data.remote.RemoteUserRepository
-import com.example.androidapp.data.remote.RetrofitClient
 import com.example.androidapp.databinding.FragmentUserBinding
 import com.example.androidapp.repository.UserRepository
+import com.example.androidapp.viewmodel.UserViewModel
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class UserFragment : Fragment() {
     private var _binding: FragmentUserBinding? = null
     private val binding get() = _binding!!
-    private val userRepository: UserRepository = RemoteUserRepository()
+    private val viewModel: UserViewModel by viewModels()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUserBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,31 +38,25 @@ class UserFragment : Fragment() {
         binding.tvFollowingEdit.text = "편집"
 
 
+        // viewModel에 요청(id 1번의 사용자 한 명 데이터)
         viewLifecycleOwner.lifecycleScope.launch {
-            try {
-                val user = userRepository.getUser(1)
-
-                binding.tvNickname.text = "${user.first_name} ${user.last_name}"
-
-                binding.ivProfile.load(user.avatar) {
-                    crossfade(true)
-                    placeholder(R.drawable.circle_gray_bg)
-                    transformations(CircleCropTransformation())
+            viewModel.user.collectLatest { user ->
+                user?.let {
+                    binding.tvNickname.text = "${it.first_name} ${it.last_name}"
+                    binding.ivProfile.load(it.avatar) {
+                        crossfade(true)
+                        placeholder(R.drawable.circle_gray_bg)
+                        transformations(CircleCropTransformation())
+                    }
                 }
-
-            } catch (e: Exception) {
-                binding.tvNickname.text = "닉네임 불러오기 실패"
             }
+        }
 
-            try {
-                val userList = userRepository.getUserList(1)
-
+        // ViewModel에게 요청(팔로우하고 있는 user 여러 명의 데이터)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.userList.collectLatest { userList ->
                 binding.rvFollowing.adapter = FollowingAdapter(userList)
-
                 binding.tvFollowingTitle.text = "팔로잉 (${userList.size})"
-
-            } catch (e: Exception) {
-                binding.tvNickname.text = "불러오기 실패"
             }
         }
 
@@ -60,7 +64,6 @@ class UserFragment : Fragment() {
         binding.ivBenefitArrow.setOnClickListener { }
         binding.tvFollowingEdit.setOnClickListener { }
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
