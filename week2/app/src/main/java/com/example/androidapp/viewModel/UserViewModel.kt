ae8cb3bbf7a2510a31ae3be4e4196a625735e78f
pkyujin <luxoris02@sookmyung.ac.kr>
2026-05-09 21:05:27 +0900

    [Refactor(제이/박유진)] 6주차 HiltViewModel 구현

--- week2/app/src/main/java/com/example/androidapp/viewModel/UserViewModel.kt
+++ week2/app/src/main/java/com/example/androidapp/viewModel/UserViewModel.kt
@@ -3,15 +3,17 @@ package com.example.androidapp.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.androidapp.data.model.UserData
-import com.example.androidapp.data.remote.RemoteUserRepository
 import com.example.androidapp.data.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class UserViewModel : ViewModel() {
-    private val repository: UserRepository = RemoteUserRepository()
-
+@HiltViewModel
+class UserViewModel @Inject constructor(
+    private val repository: UserRepository
+) : ViewModel() {
     private val _user = MutableStateFlow<UserData?>(null)
     val user: StateFlow<UserData?> = _user
 
